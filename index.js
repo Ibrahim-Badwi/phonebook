86b3191c5431a0e4880657dd75adbfbd10f994e0
Ibrahim-Badwi <ib.a.badwi@gmail.com>
2023-09-17 21:17:30 +0300

    add dist folder

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static('dist'))
 
 /* morgan assignBody middleware */
 const assignBody = (request, response, next) => {
